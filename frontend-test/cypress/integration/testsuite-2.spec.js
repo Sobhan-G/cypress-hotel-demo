9c89e84a72364a43a9f16f08c1b9a811b8fb5670
Sobhan Gilani <Gilani.sobha@gmail.com>
2021-11-04 20:28:30 +0100

    ok

--- frontend-test/cypress/integration/testsuite-2.spec.js
+++ frontend-test/cypress/integration/testsuite-2.spec.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
 
 
-// testsuite-1
+// testsuite-2
 
-describe('perfom a valid login and create a room', function(){
+describe('login and create a new client and logout', function(){
 
-    it('testcase-1', function(){
+    it('testcase-2', function(){
 
       cy.visit('http://localhost:3000/login?redirect=%2Flogin')
       cy.title('Testers Hotel')
@@ -19,24 +19,19 @@ describe('perfom a valid login and create a room', function(){
       cy.wait(1000)
       cy.contains('Tester Hotel Overview')
       cy.wait(1000)
-      cy.get(':nth-child(1) > .btn').click()
+      cy.get('.blocks > :nth-child(2) > .btn').click()
+      cy.contains('Create Client')
       cy.get('h2 > .btn').click()
-      cy.contains('New Room')
-      cy.get(':nth-child(1) > select').select('double')
-      cy.get(':nth-child(2) > input').type('2')
-      cy.get(':nth-child(3) > input').type('10')
-      cy.get(':nth-child(5) > input').type('9500')
-      cy.get('[value="penthouse"]')
+      cy.get(':nth-child(1) > input').type('Sobhan')
+      cy.get(':nth-child(2) > input').type('Gilani.sobhan@gmail.com')
+      cy.get(':nth-child(3) > input').type('073-5950450')
       cy.get('.blue').click()
       cy.wait(1000)
       cy.get(':nth-child(3) > .btn').click()
       cy.wait(1000)
       cy.get('.user > .btn').click()
-      cy.wait(1000)
-      cy.contains('Login')
-
-
-
+      cy.contains('Login') 
+      
       
      
     })
